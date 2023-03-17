57a151d9a35ae5fcc290bd91ffb6b15718ff9c3b
Admin <admin@admins-mac-mini.home>
2023-03-17 19:05:57 +0100

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <App />
+    ),
+  },
+  {
+    path: "login",
+    element: <div>Login</div>,
+  },
+]);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
